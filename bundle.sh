458753f1d4bc26f27943f8f1ddc1b64da14c2faa
Peter Širka <petersirka@gmail.com>
2022-03-09 18:16:51 +0100

    Added new improvements.

--- bundle.sh
+++ bundle.sh
@@ -9,7 +9,6 @@ cp -a ../modules/ modules
 cp -a ../schemas/ schemas
 cp -a ../public/ public
 cp -a ../views/ views
-cp -a ../resources/default.resource resources/default.resource
 total4 --bundle ../app.bundle
 cd ..
 rm -rf .bundle
